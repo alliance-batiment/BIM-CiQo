609df801a1a1be5f50f62aae33a38b9a4d36106f
Arnaud LACROIX <arnaud.lacroix@triazur.com>
2021-12-28 07:55:34 +0000

    Feat/psa 96 unit test implementation

---
+++ src/Tests/demo.test.js
@@ -0,0 +1,12 @@
+const { IntType } = require("three");
+
+describe("These are examples", () => {
+  it("should return 3", () => expect(1 + 2).toEqual(3));
+
+  it("should not return 5", () => expect(1 + 2).not.toEqual(5));
+
+  it("should return undefined", () => {
+    const z = 0;
+    expect(z).toBeFalsy();
+  });
+});
