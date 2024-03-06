79d5df085bc1a7727b4ec6982e3900c252f0a705
triazur <guillaume.cassin@triazur.com>
2024-03-06 13:38:29 +0100

    save

---
+++ module-federation/scripts/overrides/webpack-config.js
@@ -0,0 +1,21 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+const webpackConfigPath = 'react-scripts/config/webpack.config';
+const webpackConfig = require(webpackConfigPath);
+
+const override = (config) => {
+  config.plugins.push(new ModuleFederationPlugin(require('../../../modulefederation.config.js')));
+
+  config.output = {
+    path: path.join(__dirname, "../../../build-federation"),
+    publicPath: '/',
+    filename: "bundle.js",
+  };
+
+  return config;
+};
+
+require.cache[require.resolve(webpackConfigPath)].exports = (env) => override(webpackConfig(env));
+
+module.exports = require(webpackConfigPath);
---
+++ module-federation/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const template = (
+    <div>
+        <h1>Microfrontend Template</h1>
+    </div>
+);
+
+ReactDOM.render(
+    template,
+    document.querySelector(
+        '#fromjs'
+    )
+);
